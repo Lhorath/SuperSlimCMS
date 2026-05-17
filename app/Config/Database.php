df5372869b54c4942f309dbe0f939b41ed0863fe
Zachary MacPhee <zacharymacphee@hotmail.ca>
2026-05-17 11:43:30 -0300

    w
    w

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,9 +26,9 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
+        'hostname'     => '127.0.0.1',
+        'username'     => 'website',
+        'password'     => 'dax123',
         'database'     => '',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
